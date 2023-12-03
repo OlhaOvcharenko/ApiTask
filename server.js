b47b01d6c8ce4ee1f7ae5369031a5a36300c74f6
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-12-03 13:34:36 +0300

    add client`s files add client`s files

--- server.js
+++ server.js
@@ -11,15 +11,16 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use('/api', testimonialsRoutes); 
-app.use('/api', concertsRoutes); 
-app.use('/api', seatsRoutes); 
+
+// Używaj różnych ścieżek dla różnych tras
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
 
 app.use((req, res) => {
-  res.status(404).json({message: 'Not found...'});
+  res.status(404).json({ message: 'Not found...' });
 });
-   
- 
+
 const port = 8000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
