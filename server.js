7f91c5b1aab74db09bae31e80f23c905a9c4f905
unknown <ovcharenko.olga.2014@gmail.com>
2023-12-16 15:37:26 +0300

    add mongoose

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 
 // import routes
-const testimonialRoutes = require('./routes/testimonials.routes');
-const concertsRoutes = require('./routes/concerts.routes');
-const seatsRoutes = require('./routes/seats.routes');
+//const testimonialRoutes = require('./routes/testimonials.routes');
+//const concertsRoutes = require('./routes/concerts.routes');
+//onst seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
 
@@ -16,6 +17,15 @@ app.use(express.json());
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
+
+mongoose.connect('mongodb://0.0.0.0:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
+
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
@@ -29,9 +39,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api', testimonialRoutes); 
-app.use('/api', concertsRoutes); 
-app.use('/api', seatsRoutes); 
+//app.use('/api', testimonialRoutes); 
+//app.use('/api', concertsRoutes); 
+//app.use('/api', seatsRoutes); 
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
