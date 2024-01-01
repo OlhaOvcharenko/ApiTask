e01a5e272a2cc2d470a6db48cee191a160a3a56e
unknown <ovcharenko.olga.2014@gmail.com>
2024-01-01 15:26:30 +0300

    add new endpoints for concerts

--- controllers/concerts.controller.js
+++ controllers/concerts.controller.js
@@ -2,7 +2,9 @@ const Concert = require('../models/concert.model');
 
 exports.getAll = async(req, res) => {
   try {
-    res.json(await Concert.find());
+    const concerts = await Concert.find();
+    //console.log("All concerts", concerts)
+    res.json(concerts);
   } catch (err) {
     res.status(500).json({ message: err });
   }
@@ -23,13 +25,18 @@ exports.getRandom = async(req, res) => {
 exports.getById =  async(req, res) => {
   try {
     const concert = await Concert.findById(req.params.id);
-    if(!concert)res.status(404).json({message: 'Not found..'});
-    else res.json(concert);
-  }
-  catch(err) {
+
+    if(!concert) { 
+      res.status(404).json({message: 'Not found..'});
+    } else {
+      res.json(concert);
+    }
+
+  } catch(err) {
     res.status(500).json({ message: err });
   }
 };
+
 
 exports.addNewConcert = async (req, res) => {
   try {
@@ -73,5 +80,80 @@ exports.deleteConcert = async (req, res) => {
   }
   catch(err) {
     res.status(500).json({ message: err });
+  }
+};
+
+
+//new controllers
+
+exports.getByPerformer = async (req, res) => {
+  try {
+    const performer = req.params.performer.replace(/\+/g, ' ');
+    const concertsByPerformer = await Concert.find({
+      performer: { $regex: new RegExp(`^${performer}$`, 'i') }
+    });
+
+    if (!concertsByPerformer.length === 0) {
+      res.status(404).json({ error: 'Concers are not found' });
+    } else {
+      res.json({ concerts: concertsByPerformer });
+    }
+
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error', message: error.message });
+  }
+};
+
+exports.getByGenre = async (req, res) => {
+  const { genre } = req.params;
+  try {
+    const concertsByGenre = await Concert.find({ genre: { $regex: new RegExp(genre, 'i') } });
+
+    if (concertsByGenre.length === 0) {
+      res.status(404).json({message: 'Not found..'});
+    } else {
+      res.json({concertsByGenre});
+    }
+
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error', message: error.message });
+  }
+};
+
+exports.getByPrices = async (req, res) => {
+  const { price_min, price_max } = req.params;
+
+  try {
+    const concertsByPrices = await Concert.find({
+      $and: [
+        { price: { $gte: Number(price_min) } },
+        { price: { $lte: Number(price_max) } }
+      ]
+    });
+
+    if (!concertsByPrices || concertsByPrices.length === 0) {
+      res.status(404).json({ message: 'No concerts found.' });
+    } else {
+      res.json({ concertsByPrices });
+    }
+
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error', message: error.message });
+  }
+};
+
+exports.getByDay = async (req, res) => {
+  const { day } = req.params;
+  try {
+    const concertsByDay = await Concert.find({ day: Number(day) });
+
+    if (!concertsByDay&&concertsByDay.length === 0) {
+      res.status(404).json({ message: 'No concerts found for the specified day.' });
+    } else {
+      res.json({ concertsByDay });
+    }
+
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error', message: error.message });
   }
 };
--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -4,9 +4,16 @@ const ConcertController = require('../controllers/concerts.controller')
 
 router.get('/concerts', ConcertController.getAll);
 router.get('/concerts/random', ConcertController.getRandom);
+
+//new endpoints 
+router.get('/concerts/genre/:genre', ConcertController.getByGenre);
+router.get('/concerts/performer/:performer', ConcertController.getByPerformer);
+router.get('/concerts/price/:price_min/:price_max', ConcertController.getByPrices);
+router.get('/concerts/day/:day', ConcertController.getByDay);
+
 router.get('/concerts/:id', ConcertController.getById);
 router.post('/concerts', ConcertController.addNewConcert);
 router.put('/concerts/:id', ConcertController.updateConcert);
-router.delete('/concerts/:id', ConcertController.deleteConcert)
+router.delete('/concerts/:id', ConcertController.deleteConcert);
 
 module.exports = router;
