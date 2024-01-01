726b2e3c455a111ff18fa0e0e9f62291837217d9
unknown <ovcharenko.olga.2014@gmail.com>
2024-01-01 17:46:09 +0300

    fix connection with database

--- server.js
+++ server.js
@@ -11,39 +11,41 @@ const uri = 'mongodb+srv://ovcharenkoolga2014:Soul1998@cluster0.jwymdrz.mongodb.
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
 
-if(NODE_ENV === 'production') dbUri = uri;
-else if(NODE_ENV === 'test') dbUri = 'mongodb://0.0.0.0:27017/NewWaveDB';
+if (NODE_ENV === 'production') dbUri = uri;
+else if (NODE_ENV === 'test') dbUri = 'mongodb://0.0.0.0:27017/NewWaveDBtest';
 else dbUri = 'mongodb://0.0.0.0:27017/NewWaveDB';
 
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbUri);
 const db = mongoose.connection;
-
 
 db.once('open', () => {
   console.log('Connected to the database');
 });
 db.on('error', err => console.log('Error ' + err));
 
-const server = app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen('8080', () => {
+  console.log('Server is running on port: 8080');
 });
 
 const io = socket(server);
-
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-	req.io = io;
-	next();
-  });
+  req.io = io;
+  next();
+});
 
-const testimonialsRoutes = require('./routes/testimonials.routes')
+const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+console.log('NODE_ENV:', NODE_ENV);
+console.log('DB URI:', dbUri);
+
+// Specify different base paths for each set of routes
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
@@ -51,17 +53,15 @@ app.use('/api', seatsRoutes);
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-  });
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 io.on('connection', socket => {
   console.log('New client! Its id – ' + socket.id);
 });
 
 app.use((req, res) => {
-    res.status(404).json({ message: 'Not found...' });
+  res.status(404).json({ message: 'Not found...' });
 });
-
-
 
 module.exports = server;
