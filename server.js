c02e43dc5d4b797b0db6d4c062faf5345098be5b
OlhaOvcharenko <134648751+OlhaOvcharenko@users.noreply.github.com>
2024-01-03 23:03:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const uri = 'mongodb+srv://ovcharenkoolga2014:Soul1998@cluster0.jwymdrz.mongodb.net/NewWaveDB?retryWrites=true&w=majority';
+const uri = `mongodb+srv://ovcharenkoolga2014:${process.env.DB_PASS}@cluster0.jwymdrz.mongodb.net/NewWaveDB?retryWrites=true&w=majority`;
 
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
